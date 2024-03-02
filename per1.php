fc0f8b839b30662c64a9c467339f3033a416fad7
febrican13 <133013707+febrican13@users.noreply.github.com>
2024-03-03 04:05:18 +0700

    Add files via upload

---
+++ per1.php
@@ -0,0 +1,11 @@
+<?php
+print "<pre>";
+print"<center><font size=14 color='red'>BIODATA</font></center>";
+echo"<hr size=4 width=60% color='blue'>";
+print "<font size=12 color='blue'>   		Nama : Febri Candra </font><br>";
+print "<font size=12 color='blue'>   		Email : febricandra33@gmail.com </font><br>";
+print "<font size=12 color='blue'>   		Jurusan : Teknik informatika </font><br>";
+print "<font size=12 color='blue'>   		hoby : Traveling </font><br></pre>";
+echo "<hr size=4 width=60% color='blue'>";?>
+</body>
+</html>
